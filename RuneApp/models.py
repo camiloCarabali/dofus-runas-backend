d3d8994673907f0e480e20073be04b9027f9e7eb
camiloCarabali <73615948+camiloCarabali@users.noreply.github.com>
2023-11-27 22:43:13 -0500

    Models and Settings
    The database model was created and after this the corresponding configuration was made so that it is deployed.

--- RuneApp/models.py
+++ RuneApp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100, null=False)
+
+    class Meta:
+        db_table = 'Category'
+
+    def __str__(self):
+        return self.Name
+
+
+class Element(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100, null=False)
+
+    class Meta:
+        db_table = 'Element'
+
+    def __str__(self):
+        return self.Name
+
+
+class Rune(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100, null=False)
+    Density = models.IntegerField(null=False)
+    Image = models.CharField(max_length=200)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    Element = models.ForeignKey(Element, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Rune'
+
+    def __str__(self):
+        return self.Name
+
