d617cd39f4b510b92c99a8b8aabfbf066e995496
camiloCarabali <73615948+camiloCarabali@users.noreply.github.com>
2024-01-10 10:16:08 -0500

    cambios en el modelo

--- RuneApp/models.py
+++ RuneApp/models.py
@@ -14,12 +14,12 @@ class Category(models.Model):
         return self.Name
 
 
-class Element(models.Model):
+class Characteristic(models.Model):
     ID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=100, null=False)
 
     class Meta:
-        db_table = 'Element'
+        db_table = 'Characteristic'
 
     def __str__(self):
         return self.Name
@@ -28,14 +28,16 @@ class Element(models.Model):
 class Rune(models.Model):
     ID = models.AutoField(primary_key=True)
     Name = models.CharField(max_length=100, null=False)
-    Density = models.IntegerField(null=False)
+    Density = models.IntegerField(null=True)
+    level = models.IntegerField(null=False)
+    Efect = models.IntegerField(null=True)
     Image = models.CharField(max_length=200)
+    OverMax = models.IntegerField(null=True)
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    Element = models.ForeignKey(Element, on_delete=models.CASCADE)
+    Characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'Rune'
 
     def __str__(self):
         return self.Name
-
